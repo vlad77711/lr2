6afa01146a46d191a95728d038e93adf11298b9a
vlad77711 <46203226+vlad77711@users.noreply.github.com>
2021-03-15 14:10:06 +0200

    lr2

---
+++ lr2/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lr2
+{
+    class plane
+    {
+        public string Name_plane;
+        public string Country_departure;
+        public string Country_arrival;
+        public int Flight_number;
+        public int pepolnumber_of_seats;
+        public int lux_seats;
+        public bool eat;
+        public int Getseats()
+        {
+            return pepolnumber_of_seats + lux_seats;
+        }
+    }
+};
